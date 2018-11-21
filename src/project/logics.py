01260a6ce8ab1d0f972d301fba55091f75f8001c
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-11-20 21:32:23 -0300

    filter by active companies and swagger stage url

--- src/project/logics.py
+++ src/project/logics.py
@@ -1,10 +1,14 @@
-from project.models import Company
+from sqlalchemy.sql.expression import true
+
+from project.models import Company, UserCompanies
 from project.serializers import CompanySerializer
 
 
 class CompanyLogics:
     def list_by_user(self, user):
         companies = Company.query.join(Company.users, aliased=True)\
-                    .filter_by(user_id=user.id)
+                    .filter(
+                        UserCompanies.user_id == user.id,
+                        Company.active == true())
 
         return CompanySerializer.to_array(companies)
