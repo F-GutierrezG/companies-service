c8810a2516caf9ee913cd28b3617930ee76646cc
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-11-22 17:15:17 -0300

    IPs updated

--- stage-deploy.sh
+++ stage-deploy.sh
@@ -19,7 +19,7 @@ ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -e 'POSTGR
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -e 'API_URL=definitions/swagger.yml' -p 8082:8080 --name companies-swagger --network companies-service-network --ip 172.21.0.3 $REGISTRY_REPO/$SWAGGER:$TAG"
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -e 'FLASK_ENV=development' -e 'FLASK_APP=manage.py' -e 'APP_SETTINGS=project.config.DevelopmentConfig' -e 'DATABASE_URL=postgres://postgres:postgres@companies-db:5432/companies' -e 'DATABASE_TEST_URL=postgres://postgres:postgres@companies-db:5432/companies_test' -e 'SECRET_KEY=secret_key' -e USERS_SERVICE_URL=$USERS_SERVICE_URL -p 5002:5000 --name companies --network companies-service-network --ip 172.21.0.2 $REGISTRY_REPO/$COMPANIES:$TAG"
 
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect client-network --ip 172.18.0.7 companies'
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect client-network --ip 172.18.0.8 companies'
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect client-network --ip 172.18.0.9 companies-swagger'
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container exec companies python manage.py recreate-db'
