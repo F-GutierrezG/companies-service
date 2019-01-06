0ea953d61f301bba78d0e94acf7a67877232a2c9
Carlos Silva <anklosr@gmail.com>
2019-01-06 15:40:18 -0300

    Authenticate classifications

--- src/project/views/classifications.py
+++ src/project/views/classifications.py
@@ -1,4 +1,6 @@
 from flask import Blueprint
+
+from auth.decorators import authenticate
 from project.views.utils import success_response
 
 from project.logics import ClassificationLogics
@@ -7,7 +9,8 @@ from project.logics import ClassificationLogics
 classifications_blueprint = Blueprint('classifications', __name__)
 
 @classifications_blueprint.route('/companies/classifications', methods=['GET'])
-def list():
+@authenticate
+def list(user):
     classifications = ClassificationLogics().list()
 
     return success_response(
